81a908032fa1bc80498b95909487d489d878a73c
shahddtarek <148455577+shahddtarek@users.noreply.github.com>
2024-12-20 04:13:29 +0200

    Add files via upload

---
+++ lms/src/main/java/com/project/lms/Service/QuizService.java
@@ -0,0 +1,86 @@
+package com.project.lms.Service;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.project.lms.Repository.*;
+import com.project.lms.Entity.*;
+
+@Service
+public class QuizService {
+
+    @Autowired
+    private QuizRepository quizRepository;
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private SubmissionRepository submissionRepository;
+// Create Quiz
+    public Quiz createQuiz(Long courseId, Quiz quiz) {
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new RuntimeException("Course not found"));
+        quiz.setCourse(course);
+        return quizRepository.save(quiz);
+    }
+
+
+    public Submission submitQuiz(Long quizId, String studentUsername) {
+       
+        boolean quizExists = quizRepository.existsById(quizId);
+        if (!quizExists) {
+            throw new RuntimeException("Quiz not found");
+        }
+        User student = userRepository.findByUsername(studentUsername);
+        if (student==null || !"student".equals(student.getRole())) {
+                throw new RuntimeException("User is not a student");
+        } 
+        Submission submission = new Submission();;
+        submission.setQuizId(quizId);
+        submission.setStudentUsername(studentUsername);
+        submission.setStudentId(student.getId());
+       
+
+               // Check if the student already submitted
+        List<Submission> existingSubmissions = submissionRepository.findByQuizId(quizId);
+        boolean alreadySubmitted = existingSubmissions.stream()
+            .anyMatch(sub -> sub.getStudentId().equals(submission.getStudentId()));
+        if (alreadySubmitted) {
+            throw new RuntimeException("Student has already submitted this quiz");
+        }
+        return submissionRepository.save(submission);
+    }
+    public Submission assignScore(Long QuizId , Long StudentId , Double Score , String Feedback) { // 
+        // Find the submission
+        Submission submission = submissionRepository.findByQuizIdAndStudentId(QuizId,StudentId)
+                .orElseThrow(() -> new RuntimeException("Submission not found"));
+
+        submission.setScore(Score);
+        submission.setFeedback(Feedback);
+
+        // Save and return the updated submission
+        return submissionRepository.save(submission);
+    }
+    
+    public Submission getStudentScoreAndFeedback(Long quizId, String studentUsername) {
+        User student = userRepository.findByUsername(studentUsername);
+        return submissionRepository.findByQuizIdAndStudentId(quizId, student.getId())
+                .orElseThrow(() -> new RuntimeException("Submission not found"));
+    }
+
+    // Fetch all quizzes for a course
+    public List<Quiz> getQuizzesByCourse(Long courseId) {
+        return quizRepository.findByCourseId(courseId);
+    }
+
+    // Get Quiz Details
+    public Quiz getQuizById(Long quizId) {
+        return quizRepository.findById(quizId)
+                .orElseThrow(() -> new RuntimeException("Quiz not found"));
+    }
+
+}
