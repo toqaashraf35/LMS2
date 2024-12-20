a7e2ed7e82bd84944321b0e3f992e7a6253e96d6
shahddtarek <148455577+shahddtarek@users.noreply.github.com>
2024-12-20 04:05:24 +0200

    Add files via upload

---
+++ lms/src/main/java/com/project/lms/Entity/Quiz.java
@@ -0,0 +1,46 @@
+package com.project.lms.Entity;
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+}
